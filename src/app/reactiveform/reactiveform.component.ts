3faf1689f5d6bbbf69721138ae7b64ad46f1c1ff
Durga Ganesh <akulaganesh003@gmail.com>
2022-08-29 08:50:47 +0530

    reactive form with validation

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactiveform',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveformComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  registerForm!: FormGroup;
+  submitted = false;
+ 
+  constructor(private formBuilder: FormBuilder) {}
+ 
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+ 
+  onSubmit() {
+    this.submitted = true;
+ 
+    // stop the process here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+ 
+    alert('SUCCESS!!');
   }
 
 }
